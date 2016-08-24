7e74c5779f7c3f6f93ca0ddf9f438110a56db73f
Colin Goutte <cgte@bk.ru>
2016-08-25 00:21:38 +0200

    Add i3 and conky packages

--- apt.sh
+++ apt.sh
@@ -4,4 +4,5 @@ sudo apt-get install iceweasel icedove vim-nox git gitk gitg giggle geany \
 		     gedit thunar python-dev python3-dev python-pip git gitk tig openssh-client openssh-server \
  		     terminator gcc g++ htop  iotop vim git gitk giggle tig tree postgresql-client \
 		     xclip  mysql-client mysql-utilities  libmysqlclient-dev python-dev nodejs npm libpq-dev aptitude \
-		     libpq-dev  libffi-dev gitg gitg tig gitk hexchat docker.io rsync 
+		     libpq-dev  libffi-dev gitg gitg tig gitk hexchat docker.io rsync i3 conky
+
