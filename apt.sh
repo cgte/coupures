fece7c74f8241233fd0d5c14d394e997a74010b8
Colin Goutte <cgte@bk.ru>
2016-08-24 23:52:49 +0200

    Add scripts to install packages

---
+++ apt.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install iceweasel icedove vim-nox git gitk gitg giggle geany \
+		     gedit thunar python-dev python3-dev python-pip git gitk tig openssh-client openssh-server \
+ 		     terminator gcc g++ htop  iotop vim git gitk giggle tig tree postgresql-client \
+		     xclip  mysql-client mysql-utilities  libmysqlclient-dev python-dev nodejs npm libpq-dev aptitude \
+		     libpq-dev  libffi-dev gitg gitg tig gitk hexchat docker.io rsync 
---
+++ pip.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo pip install --upgrade pip virtualenv virtualkeyring setuptools autopep8 pylint flake8 psycopg2
+sudo pip install bcrypt
