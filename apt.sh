79441362fbc668263090d1a8eace77d5ef9c3e93
Colin Goutte <cgte@bk.ru>
2016-08-25 14:02:55 +0200

    Add packages for i3

--- apt.sh
+++ apt.sh
@@ -4,5 +4,12 @@ sudo apt-get install iceweasel icedove vim-nox git gitk gitg giggle geany \
 		     gedit thunar python-dev python3-dev python-pip git gitk tig openssh-client openssh-server \
  		     terminator gcc g++ htop  iotop vim git gitk giggle tig tree postgresql-client \
 		     xclip  mysql-client mysql-utilities  libmysqlclient-dev python-dev nodejs npm libpq-dev aptitude \
-		     libpq-dev  libffi-dev gitg gitg tig gitk hexchat docker.io rsync i3 conky
+		     libpq-dev  libffi-dev gitg gitg tig gitk hexchat docker.io rsync i3 conky \
+             rxvt-unicode-256color lightdm x11-xserver-utils \
+             curl alsa-utils \
+             mpd mpc ncmpcpp \
+             notification-daemon xinput \
+             firmware-iwlwifi \
+
+
 
